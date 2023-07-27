671b64d18c234740635f156eb890ee7bee103b35
Flavio <flavio.a.618@gmail.com>
2023-07-27 09:48:03 +0200

    Improved macros learnlist template

--- macros/learnlist-template.js
+++ macros/learnlist-template.js
@@ -8,17 +8,23 @@
 const macros = utils.macros;
 
 macros['learnlist template 1-2'] = function(str) {
+    if (!str.includes("__NOTOC__")) {
+        str = "__NOTOC__" + str;
+    }
+
     return str
-        .replace(/{{Learnlist\/(\w+)h\|([^|]+)\|(\w+)\|(\w+)\|(\d)\|(\d)}}/gi,
+        .replace(/{{Learnlist\/(\w+)h\|([^|]+)\|(\w+)\|(\w+)\|(\d)\|(\d)\s*(\|[^}]+)?}}/gi,
             "{{#invoke: learnlist/hf | $1h | $2 | $3 | $4 | $5 | $6 }}\n{{#invoke: render | render | Modulo:Learnlist/entry$5 | $1 | //")
-        .replace(/{{Learnlist\/(\w+)f\|([^|]+)\|(\w+)\|(\w+)\|(\d)\|(\d)}}/gi,
+        .replace(/{{Learnlist\/(\w+)f\|([^|]+)\|(\w+)\|(\w+)\|(\d)\|(\d)\s*(\|[^}]+)?}}/gi,
             "}}\n{{#invoke: learnlist/hf | $1f | $2 | $3 | $4 | $5 | $6 }}")
         // Of course breed is fucked up
         .replace(/{{MS\|(\d{3})\|[^}]+}}/g, "#$1#")
+        .replace(/{{#invoke:\s*MiniSprite\s*\|[\w\s]+\|\s*(\d{3})[^}]+}}/g, "#$1#")
         .replace(/{{[Ll]earnlist\/\w+\d\|([^}]*)}}/g, "|$1| //")
         // This replacement happens after the others, so it can remove the render call
         .replace(/{{#invoke: render \| render \| Modulo:Learnlist\/entry\d \| \w+ \| \/\/\n{{learnlist\/(\w+)(\d)null}}\s*\n}}/g,
             "{{#invoke: learnlist/entry$2 | $1null }}")
+        .replace(/\b([cC])oleot\b/g, "$1oleottero")
     ;
 };
 
@@ -127,19 +133,8 @@ macros['learnlist template 3-4'] = function(str) {
             .replace(/{{learnlist\/level([34])\|(\w+)\|([^|]+)\|([^}]*)\|([^}|]*)}}\n?/gi, "");
     }
 
-    return str
-        .replace(/{{Learnlist\/(\w+)h\|([^|]+)\|(\w+)\|(\w+)\|(\d)\|(\d)}}/gi,
-            "{{#invoke: learnlist/hf | $1h | $2 | $3 | $4 | $5 | $6 }}\n{{#invoke: render | render | Modulo:Learnlist/entry$5 | $1 | //")
-        .replace(/{{Learnlist\/(\w+)f\|([^|]+)\|(\w+)\|(\w+)\|(\d)\|(\d)}}/gi,
-            "}}\n{{#invoke: learnlist/hf | $1f | $2 | $3 | $4 | $5 | $6 }}")
-        // Of course breed is fucked up
-        .replace(/{{MS\|(\d{3})\|[^}]+}}/g, "#$1#")
-        .replace(/{{[Ll]earnlist\/\w+\d\|([^}]*)}}/g, "|$1| //")
-        // This replacement happens after the others, so it can remove the render call
-        .replace(/{{#invoke: render \| render \| Modulo:Learnlist\/entry\d \| \w+ \| \/\/\n{{learnlist\/(\w+)(\d)null}}\s*\n}}/g,
-            "{{#invoke: learnlist/entry$2 | $1null }}")
-        .replace(/\b([cC])oleot\b/g, "$1oleottero")
-    ;
+    str = macros['learnlist template 1-2'](str);
+    return str;
 };
 
 
